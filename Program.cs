3e4f15fb51e6ea200e984ea007f78005716219d8
mohammed-alshabibi <mohammed1212alshabibi@gmail.com>
2025-04-15 16:24:46 +0400

    add top freq code

--- Program.cs
+++ Program.cs
@@ -4,7 +4,38 @@
     {
         static void Main(string[] args)
         {
-            
+            List <int> numbers = new List<int>();
+            List<int> duplicates = new List<int>();
+            Console.WriteLine("Enter 6 numbers :");
+            for (int i = 0; i < 6; i++)
+            {
+                Console.WriteLine("Enter "+ i+1 +" number :");
+                int number = int.Parse(Console.ReadLine());
+                numbers.Add(number);
+            }
+            int frequency = 0;
+            int maxFreq = 0;
+            int FirstElement = numbers[0];
+            for (int i = 0; i<numbers.Count; i++)
+            {
+                for (int j = i + 1; j < numbers.Count; j++)
+                {
+                    if (numbers[i] == numbers[j])
+                    {
+                        frequency++;
+                    }
+                }
+            }
+            if (frequency == 0)
+            {
+                Console.WriteLine("All numbers are unique.");
+            }
+            else
+            {
+
+                Console.WriteLine("There are " + frequency + " duplicate numbers."+ duplicates);
+            }
         }
+
     }
 }
